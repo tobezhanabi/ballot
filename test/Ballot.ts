7b8bcbb5475720cb3a8cfeac6f8ec9dbde2c7399
tobenna <eliyahscopy@gmail.com>
2023-08-04 12:38:22 +0100

    2 commit

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -1,5 +1,8 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { Ballot } from "../typechain-types";
+
 const PROPOSALS = ["proposal 1", "proposal 2", "proposal 3"];
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
@@ -8,15 +11,37 @@ function convertStringArrayToBytes32(array: string[]) {
   }
   return bytes32Array;
 }
+
+async function deployContract() {
+  const ballotFactory = await ethers.getContractFactory("Ballot");
+  const ballotContract = await ballotFactory.deploy(
+    convertStringArrayToBytes32(PROPOSALS)
+  );
+  await ballotContract.waitForDeployment();
+  return ballotContract;
+}
 describe("Ballot", async () => {
+  let ballotContract: Ballot;
   describe("when the contract", async () => {
+    beforeEach(async () => {
+      ballotContract = await loadFixture(deployContract);
+    });
+    it("has the provided proposals", async () => {
+      for (let index = 0; index < PROPOSALS.length; index++) {
+        const proposal = await ballotContract.proposals(index);
+        expect(ethers.decodeBytes32String(proposal.name)).to.equal(
+          PROPOSALS[index]
+        );
+      }
+    });
+    it("has zero votes for all proposals", async () => {
+      for (let index = 0; index < PROPOSALS.length; index++) {
+        const proposal = await ballotContract.proposals(index);
+        expect(proposal.voteCount).to.equal("0");
+      }
+    });
     it("should set deployer address as chairperson", async () => {
       const accounts = await ethers.getSigners();
-      const ballotFactory = await ethers.getContractFactory("Ballot");
-      const ballotContract = await ballotFactory.deploy(
-        convertStringArrayToBytes32(PROPOSALS)
-      );
-      await ballotContract.waitForDeployment();
       const chairperson = await ballotContract.chairperson();
       expect(chairperson).to.eq(accounts[0].address); // because we are using hardhat, the first address is the deployer.
     });
