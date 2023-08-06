31a942f7b18b762337554ed8dcff11a6235eb122
tobenna <eliyahscopy@gmail.com>
2023-08-06 07:44:53 +0100

    new

--- scripts/deployWithEther.ts
+++ scripts/deployWithEther.ts
@@ -1,5 +1,8 @@
 import { ethers } from "ethers";
-
+import { Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+dotenv.config();
+// contract 0xec16A8B63111Bcde389D72668Cc5453761b64988
 async function main() {
   const proposals = process.argv.slice(2);
   console.log("Deploying Ballot contract");
@@ -7,7 +10,12 @@ async function main() {
   proposals.forEach((element, index) => {
     console.log(`Proposal N. ${index + 1}: ${element}`);
   });
-  const ballotFactory = await ethers.getContractFactory("Ballot");
+  const provider = new ethers.JsonRpcProvider(
+    process.env.ALCHEMY_API_KEY ?? ""
+  );
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+
+  const ballotFactory = new Ballot__factory(wallet); // this is gotten from typechain
   const ballotContract = await ballotFactory.deploy(
     proposals.map(ethers.encodeBytes32String)
   );
